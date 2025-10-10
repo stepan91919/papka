ccde3d2290c287ecf5d018994d8968bdc7113d1e
stepan91919 <crazyddogy801@gmail.com>
2025-10-10 12:19:08 +0300

    123
    13542354

---
+++ ConsoleApp19/ConsoleApp19/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            double b = 5.1;
+            int sum = a + (int) b;
+            double num1 = b + a;
+            
+            Console.WriteLine(num1);
+        }
+    }
+}
